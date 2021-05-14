3d5456da136348d1f2da8024a68fbe31a0100606
lymarrie <16665882+lymarrie@users.noreply.github.com>
2021-05-14 14:02:47 -0400

    updated to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ data }) {
             border-bottom: 1px solid;
           `}
         >
-          Amazing Pandas Eating Things
+          Amazing Pandas - and Luc - Eating Things
         </h1>
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
