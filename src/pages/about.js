02ef35b68a2e7b8a829c0723095b1ca2dda694d9
lymarrie <16665882+lymarrie@users.noreply.github.com>
2021-05-14 13:21:28 -0400

    about page update

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ export default function About({ data }) {
       <h1>About {data.site.siteMetadata.title}</h1>
       <p>
         We're the only site running on your computer dedicated to showing the
-        best photos and videos of pandas eating lots of food. Also Luc is a Gatsby pro.
+        best photos and videos of pandas eating lots of food. Also Luc is a Gatsby pro. Before Yext, Luc was a professional NBA player.
       </p>
     </Layout>
   )
