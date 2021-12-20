956b7053bae8afd2ac5d39638edde8419aee4fb5
lymarrie <16665882+lymarrie@users.noreply.github.com>
2021-12-20 14:42:18 -0500

    another EDIT FROM THE BRANCH

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ data }) {
             border-bottom: 1px solid;
           `}
         >
-          Amazing Pandas - and amazing Lucs from the branch - Eating Things
+          Amazing Pandas - and amazing Lucs from the branch 2ND EDIT - Eating Things
         </h1>
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
