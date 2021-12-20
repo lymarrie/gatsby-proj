5f55ff66cbf4194bacfc4fcc262f43e53ec98342
lymarrie <16665882+lymarrie@users.noreply.github.com>
2021-12-20 11:00:10 -0500

    trivial commit #2

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ data }) {
             border-bottom: 1px solid;
           `}
         >
-          Amazing Pandas - and Luc - Eating Things
+          Amazing Pandas - and LUC - Eating Things
         </h1>
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
